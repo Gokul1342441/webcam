c61df8adbe6e32c76a3181d7ed859876568d83cc
Gokul1342441 <gokul333gn@gmail.com>
2024-01-18 17:27:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { WebcamImage, WebcamInitError } from 'ngx-webcam';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webcam';
+  public webcamImage: WebcamImage | null = null;
+  public errors: WebcamInitError[] = [];
+  public trigger$: Subject<void> = new Subject<void>();
+
+  public handleImage(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
 }
